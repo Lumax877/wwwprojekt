b5ec4871eb327d81a42916541b6a748e331ae08c
Lumax877 <bochta.binski7@gmail.com>
2023-11-21 23:56:23 +0100

    added models

--- projekt/managementsystem/admin.py
+++ projekt/managementsystem/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Zespol, Projekt, Zadanie, Profil
+
+admin.site.register(Zespol)
+admin.site.register(Projekt)
+admin.site.register(Zadanie)
+admin.site.register(Profil)
 
 # Register your models here.
--- projekt/managementsystem/models.py
+++ projekt/managementsystem/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Zespol(models.Model):
+    nazwa = models.CharField(max_length=100)
+    czlonkowie = models.ManyToManyField(User)
+    data_utworzenia = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nazwa
+
+class Projekt(models.Model):
+    nazwa = models.CharField(max_length=100)
+    opis = models.TextField()
+    data_rozpoczecia = models.DateField()
+    termin_wykonania = models.DateField()
+    status = models.CharField(max_length=50, choices=[('W trakcie', 'W trakcie'), ('Zakończony', 'Zakończony')])
+
+    def __str__(self):
+        return self.nazwa
+
+class Zadanie(models.Model):
+    tytul = models.CharField(max_length=100)
+    opis = models.TextField()
+    projekt = models.ForeignKey(Projekt, on_delete=models.CASCADE)
+    data_rozpoczecia = models.DateField()
+    termin_wykonania = models.DateField()
+    priorytet = models.CharField(max_length=50, choices=[('Wysoki', 'Wysoki'), ('Średni', 'Średni'), ('Niski', 'Niski')])
+    status = models.CharField(max_length=50, choices=[('Nowe', 'Nowe'), ('W trakcie', 'W trakcie'), ('Zakońcozne', 'Zakończone')])
+    przypisany_uzytkownik = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.tytul
+
+class Profil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    zespoly = models.ManyToManyField(Zespol, blank=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    def przypisane_zadania(self):
+        return Zadanie.objects.filter(przypisany_uzytkownik=self.user)
+
