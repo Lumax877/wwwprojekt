5875218087a195250ada3137aa20e4d031cdd09f
Lumax877 <bochta.binski7@gmail.com>
2023-12-10 13:43:30 +0100

    added new permission to perform status change by user

--- projekt/managementsystem/permissions.py
+++ projekt/managementsystem/permissions.py
@@ -6,8 +6,11 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return request.user and request.user.is_staff
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class CanChangeAssignedTaskPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.user == request.user
+        return obj and request.user == obj.przypisany_uzytkownik
